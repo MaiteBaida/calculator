af288a2fa6d6244a8e6a216f82d73d16f5c85a33
Maite Baida <maitebaida@Maites-MacBook-Air.local>
2024-07-10 11:45:30 -0600

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="h-14 bg-custom-black text-base flex w-full justify-center fixed bottom-0 text-custom-orange">
-      <p className="content-center">Developed by Maite Baida</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header className="h-14 bg-custom-black text-2xl flex w-full justify-center">
-      <p className="content-center">Calculator</p>
-    </header>
-  );
-}
-
-export default Header;
